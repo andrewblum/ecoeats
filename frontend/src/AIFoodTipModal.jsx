f464d3508aff7ec2ef709dd55e8a29fd757e610e
Andrew Blum <andrewdblum@gmail.com>
2024-04-27 19:00:40 -0700

    add backend gitignore

--- frontend/src/AIFoodTipModal.jsx
+++ frontend/src/AIFoodTipModal.jsx
@@ -1,4 +1,5 @@
 // modal for seeing:
 // 1 ) is the food ACTUALLy bad? how to tell if the food is bad from perplexity 
 // 2 ) if the food is actually bad, what are alternative uses i can use it for? (i e polish metal with expired ketchup)
+// 3 ) how to make the food last longer / storage tips 
 // launched via two bottons on FoodListItem
